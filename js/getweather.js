3f84627e5c4e0bbbfbd13fdf44d40b2296230254
yryzhkov <y.ryzhkov@ukr.net>
2015-07-20 14:51:25 +0300

    20.07
    pre.Final version

---
+++ js/getweather.js
@@ -0,0 +1,116 @@
+$(function() {
+	var DEG = 'C';			// C for celsius
+	var weatherTable = $('#weather'),
+		scroller = $('#scroller'),
+		location = $('p.location');
+
+	// Does this browser support geolocation?
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
+	}
+	else {
+		showError("Your browser does not support Geolocation!");
+	}
+
+	// Get user's location, and use OpenWeatherMap
+	// to get the location name and weather forecast
+
+	function locationSuccess(position) {
+
+		try {
+
+			// Retrive the cache
+			var cache = localStorage.weatherCache && JSON.parse(localStorage.weatherCache);
+			var d = new Date();
+
+			// If the cache is newer than 30 minutes, use the cache
+			if (cache && cache.timestamp && cache.timestamp > d.getTime() - 30*60*1000 ) {
+				// Get the offset from UTC (turn the offset minutes into ms)
+				var offset = d.getTimezoneOffset()*60*1000;
+				var city = cache.data.city.name;
+				var country = cache.data.city.country;
+
+				$.each(cache.data.list, function() {
+					// "this" holds a forecast object
+
+					// Get the local time of this forecast (the api returns it in utc)
+					var localTime = new Date(this.dt*1000 + offset);
+
+					addWeather(
+						this.weather[0].icon,
+						moment(localTime).calendar(),	// We are using the moment.js library to format the date
+						this.weather[0].main + ', ' + convertTemperature(this.main.temp_min) + '°' + DEG +
+												' / ' + convertTemperature(this.main.temp_max) + '°' + DEG+'</b>'
+					);
+
+				});
+
+				// Add the location to the page
+				location.html('Визначене місцезнаходження: 'city+', <b>'+country+'</b>');
+			}
+
+			else{
+			
+				// If the cache is old or nonexistent, issue a new AJAX request
+
+				var weatherAPI = 'http://api.openweathermap.org/data/2.5/forecast?lat='+position.coords.latitude+
+									'&lon='+position.coords.longitude+'&callback=?'
+
+				$.getJSON(weatherAPI, function(response){
+
+					// Store the cache
+					localStorage.weatherCache = JSON.stringify({
+						timestamp:(new Date()).getTime(),	// getTime() returns milliseconds
+						data: response
+					});
+
+					// Call the function again
+					locationSuccess(position);
+				});
+			}
+
+		}
+		catch(e){
+			showError("We can't find information about your city!");
+			window.console && console.error(e);
+		}
+	}
+
+	function addWeather(icon, day, condition){
+
+		var markup = '<tr>'+'<td>'+ day +'</td>'+'<td>'+
+            '<img src="../images/icons/'+ icon +'.png" />'+'</td>'+
+			'<td>'+ condition +'</td>'+'</tr>';
+		weatherTable.append(markup);
+	}
+
+	/* Error handling functions */
+
+	function locationError(error){
+		switch(error.code) {
+			case error.TIMEOUT:
+				showError("A timeout occured! Please try again!");
+				break;
+			case error.POSITION_UNAVAILABLE:
+				showError('We can\'t detect your location. Sorry!');
+				break;
+			case error.PERMISSION_DENIED:
+				showError('Please allow geolocation access for this to work.');
+				break;
+			case error.UNKNOWN_ERROR:
+				showError('An unknown error occured!');
+				break;
+		}
+
+	}
+
+	function convertTemperature(kelvin){
+		// Convert the temperature to either Celsius or Fahrenheit:
+		return Math.round(DEG == 'C' ? (kelvin - 273.15) : (kelvin*9/5 - 459.67));
+	}
+
+	function showError(msg){
+		weatherDiv.addClass('error').html(msg);
+	}
+
+});
